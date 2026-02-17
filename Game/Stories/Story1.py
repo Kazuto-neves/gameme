8812cd0d862217493457dc004281fd84cde7227e
Kazuto-neves <chrismneves@gmail.com>
2026-02-17 11:01:53 -0300

    Add Library_Text module and integrate story texts into Story1

--- Game/Stories/Story1.py
+++ Game/Stories/Story1.py
@@ -1,94 +1,77 @@
+from Game.Ultils.Library_Text import STORY1_TEXTS, COMMON
+
 def Story(audio_player, nome, Sujeito):
-    print(
-        '\033[7m{} {} estava voltando para sua casa quando avista uma pessoa no meio da estrada\033[m'.format(Sujeito, nome))
-    o1 = int(input(
-        '\033[4;33mOque voce faz\n 1 vai embora e deixa a pessoa para la\n 2 vai ver se ela esta viva\n digite: \033[m'))
+    print(STORY1_TEXTS["intro"].format(Sujeito, nome))
+    o1 = int(input(STORY1_TEXTS["choice1"]))
     if o1 == 1:
-        print('\033[7m{} {} ouviu o grito\033[m'.format(Sujeito, nome))
+        print(STORY1_TEXTS["choice1_1"].format(Sujeito, nome))
         audio_player.play('souseupai.mp3')
-        o111 = int(input('\033[4;33mOque voce faz\n 1 volta e para onde esta a pessoa\n 2 vai embora\n digite: \033[m'))
+        o111 = int(input(STORY1_TEXTS["choice1_1_1"]))
         if o111 == 1:
-            print('\033[7mFim de jogo\n {} foi trolado\033[m'.format(nome))
+            print(STORY1_TEXTS["choice1_1_1_1"].format(nome))
             audio_player.play('trolei.mp3')
         else:
-            print('\033[7mFim de jogo\n {} não foi trolado\033[m'.format(nome))
+            print(STORY1_TEXTS["choice1_1_1_2"].format(nome))
     else:
-        print('\033[7m{} {} ve que ele esta acordando\n\033[m'.format(Sujeito, nome))
-        o12 = int(input(
-            '\033[4;33mOque voce faz\n 1 pergunta se ele esta bem\n 2 rouba a carteira dele enquanto ele esta zonzo\n digite: \033[m'))
+        print(STORY1_TEXTS["choice1_2"].format(Sujeito, nome))
+        o12 = int(input(STORY1_TEXTS["choice1_2_1"]))
         if o12 == 1:
-            print('\033[7m{} {} pergunta se esta bem e ele responde\n\033[m'.format(Sujeito, nome))
+            print(STORY1_TEXTS["choice1_2_1_1"].format(Sujeito, nome))
             audio_player.play('aaa.mp3')
-            print('\033[7m{} {} liga para a ambulancia mas esta sem sinal\n\033[m'.format(Sujeito, nome))
-            o121 = int(input(
-                '\033[4;33mVoce tem duas alternativas\n 1 colocar o cara dentro do seu carro\n 2 tirar ele do meio da estrada e colocar na calsada \n digite: \033[m'))
+            print(STORY1_TEXTS["choice1_2_1_1_1"].format(Sujeito, nome))
+            o121 = int(input(STORY1_TEXTS["choice1_2_1_1_1_1"].format(Sujeito, nome)))
             if o121 == 1:
-                print('\033[7m Na hora que {} {} foi colocar o cara no carro ele gritou\n\033[m'.format(Sujeito, nome))
+                print(STORY1_TEXTS["choice1_2_1_1_1_1_1"].format(Sujeito, nome))
                 audio_player.play('pede.mp3')
-                print('\033[7m{} {} então percebe que ele esta alterado\n\033[m'.format(Sujeito, nome))
-                o1212 = int(
-                    input('\033[4;33mVoce faz oque\n 1 tenta conversar com ele\n 2 foje de la \n digite: \033[m'))
+                print(STORY1_TEXTS["choice1_2_1_1_1_1_1_1"].format(Sujeito, nome))
+                o1212 = int(input(STORY1_TEXTS["choice1_2_1_1_1_1_1_1_1"].format(Sujeito, nome)))
                 if o1212 == 1:
-                    print('\033[7m{} {} pergunta qual e o nome dele\n e veio a resposta\n\033[m'.format(Sujeito, nome))
+                    print(STORY1_TEXTS["choice1_2_1_1_1_1_1_1_1_1"].format(Sujeito, nome))
                     audio_player.play('naosei.mp3')
-                    print('\033[7mdigite qualquer coisa para continuar\033[m')
-                    input("")
-                    print(
-                        '\033[7m{} {} pergunta se ele sabia oque aconteceu\n e veio a resposta\n\033[m'.format(Sujeito, nome))
+                    input(COMMON["continue"])
+                    print(STORY1_TEXTS["choice1_2_1_1_1_1_1_1_1_1_1"].format(Sujeito, nome))
                     audio_player.play('naosei.mp3')
-                    print('\033[7mdigite qualquer coisa para continuar\033[m')
-                    input("")
-                    print('\033[7m{} {} pergunta se estava de carro\n e veio a resposta\n\033[m'.format(Sujeito, nome))
+                    input(COMMON["continue"])
+                    print(STORY1_TEXTS["choice1_2_1_1_1_1_1_1_1_1_1_1"].format(Sujeito, nome))
                     audio_player.play('naoseiii.mp3')
-                    print('\033[7mdigite qualquer coisa para continuar\033[m')
-                    input("")
-                    print(
-                        '\033[7m{} {} então sente um cheiro de alcool e fala voce esta empreagado\n e veio a resposta\n\033[m'.format(
-                            Sujeito, nome))
+                    input(COMMON["continue"])
+                    print(STORY1_TEXTS["choice1_2_1_1_1_1_1_1_1_1_1_1_1"].format(Sujeito, nome))
                     audio_player.play('queropova.mp3')
-                    print('\033[7mdigite qualquer coisa para continuar\033[m')
-                    input("")
-                    print('\033[7mA pessoa se altera e parte para cima do {} e acaba ficando inconciete\n\033[m'.format(
-                        nome))
-                    print('\033[7mFim de jogo\n voce não deve falar que a pessoa esta empreagada\033[m')
+                    input(COMMON["continue"])
+                    print(STORY1_TEXTS["choice1_2_1_1_1_1_1_1_1_1_1_1_1_1"].format(nome))
+                    audio_player.play('ai.mp3')
+                    print(STORY1_TEXTS["choice1_2_1_1_1_1_1_1_1_1_1_1_1_1_1"])
                 else:
-                    print('\033[7mFim de jogo\n\033[m')
+                    print(COMMON["game_over"])
                     audio_player.play('cagao.mp3')
             else:
-                print('\033[7m Na hora que {} {} foi colocar o cara no lugar seguro\n\033[m'.format(Sujeito, nome))
+                print(STORY1_TEXTS["choice1_2_1_1_1_1_2"].format(Sujeito, nome))
                 audio_player.play('pede.mp3')
-                print('\033[7m{} {} então percebe que ele esta alterado\n\033[m'.format(Sujeito, nome))
-                o122 = int(
-                    input('\033[4;33mVoce faz oque\n 1 tenta conversar com ele\n 2 foje de la \n digite: \033[m'))
+                print(STORY1_TEXTS["choice1_2_1_1_1_1_2_1"].format(Sujeito, nome))
+                o122 = int(input(STORY1_TEXTS["choice1_2_1_1_1_1_2_1_1"]))
                 if o122 == 1:
-                    print('\033[7m{} {} pergunta qual e o nome dele\n e veio a resposta\n\033[m'.format(Sujeito, nome))
+                    print(STORY1_TEXTS["choice1_2_1_1_1_1_2_1_1_1"].format(Sujeito, nome))
                     audio_player.play('naosei.mp3')
-                    print('\033[7mdigite qualquer coisa para continuar\033[m')
-                    input("")
-                    print(
-                        '\033[7m{} {} pergunta se ele sabia oque aconteceu\n e veio a resposta\n\033[m'.format(Sujeito, nome))
+                    input(COMMON["continue"])
+                    print(STORY1_TEXTS["choice1_2_1_1_1_1_2_1_1_1_1"].format(Sujeito, nome))
                     audio_player.play('naosei.mp3')
-                    print('\033[7mdigite qualquer coisa para continuar\033[m')
-                    input("")
-                    print('\033[7m{} {} pergunta se estava de carro\n e veio a resposta\n\033[m'.format(Sujeito, nome))
+                    input(COMMON["continue"])
+                    print(STORY1_TEXTS["choice1_2_1_1_1_1_2_1_1_1_1_1"].format(Sujeito, nome))
                     audio_player.play('naoseiii.mp3')
-                    print('\033[7mdigite qualquer coisa para continuar\033[m')
-                    input("")
-                    print(
-                        '\033[7m{} {} então sente um cheiro de alcool e fala voce esta empreagado\n e veio a resposta\n\033[m'.format(
-                            Sujeito, nome))
+                    input(COMMON["continue"])
+                    print(STORY1_TEXTS["choice1_2_1_1_1_1_2_1_1_1_1_1_1"].format(Sujeito, nome))
                     audio_player.play('queropova.mp3')
-                    print('\033[7mdigite qualquer coisa para continuar\033[m')
-                    input("")
-                    print('\033[7mA pessoa se altera e parte para cima do {} e acaba ficando inconciete\n\033[m'.format(
-                        nome))
-                    print('\033[7mFim de jogo\n voce não deve falar que a pessoa esta empreagada\033[m')
+                    input(COMMON["continue"])
+                    print(STORY1_TEXTS["choice1_2_1_1_1_1_2_1_1_1_1_1_1_1"].format(nome))
+                    audio_player.play('ai.mp3')
+                    print(STORY1_TEXTS["choice1_2_1_1_1_1_1_1_2_1_1_1_1_1_1"])
                 else:
-                    print('\033[7mFim de jogo\n\033[m')
+                    print(COMMON["game_over"])
                     audio_player.play('cagao.mp3')
         else:
-            print('\033[7m{} {} pega a carteira mas ouve um grito\n\033[m'.format(Sujeito, nome))
+            print(STORY1_TEXTS["choice1_2_2"].format(Sujeito, nome))
             audio_player.play('PegaLadrao.mp3')
-            print('\033[7mdigite qualquer coisa para continuar\033[m')
-            input("")
-            print('\033[7mna hora vem varios policias\nconclusão voce e preso por roubo\nfim de jogo\033[m')
+            input(COMMON["continue"])
+            print('\033[7mna hora vem varios policias\nconclusão voce e preso por roubo\033[m')
+            audio_player.play('alo-policia-federal.mp3')
+            print(COMMON["game_over"])
---
+++ Game/Ultils/Library_Text.py
@@ -0,0 +1,74 @@
+# Library_Text.py
+# This file contains all the text and input prompts used in the stories.
+
+# Story 1 Texts
+
+COMMON = {
+    "continue": "\033[7mdigite qualquer coisa para continuar\033[m",
+    "game_over": "\033[7mFim de jogo\n\033[m"
+}
+
+STORY1_TEXTS = {
+    "intro": "\033[7m{} {} estava voltando para sua casa quando avista uma pessoa no meio da estrada\033[m",
+    "choice1": "\033[4;33mOque voce faz\n 1 vai embora e deixa a pessoa para la\n 2 vai ver se ela esta viva\n digite: \033[m",
+    "choice1_1": "\033[7m{} {} ouviu o grito\033[m",
+    "choice1_1_1": "\033[4;33mOque voce faz\n 1 volta e para onde esta a pessoa\n 2 vai embora\n digite: \033[m",
+    "choice1_1_1_1": "\033[7mFim de jogo\n {} foi trolado\033[m",
+    "choice1_1_1_2": "\033[7mFim de jogo\n {} não foi trolado\033[m",
+    "choice1_2": "\033[7m{} {} ve que ele esta acordando\n\033[m",
+    "choice1_2_1": "\033[4;33mOque voce faz\n 1 pergunta se ele esta bem\n 2 rouba a carteira dele enquanto ele esta zonzo\n digite: \033[m",
+    "choice1_2_1_1": "\033[7m{} {} pergunta se esta bem e ele responde\033[m",
+    "choice1_2_1_1_1": "\033[7m{} {} liga para a ambulancia mas esta sem sinal\n\033[m",
+    "choice1_2_1_1_1_1": "\033[4;33mVoce tem duas alternativas\n 1 colocar o cara dentro do seu carro\n 2 tirar ele do meio da estrada e colocar na calsada \n digite: \033[m",
+    "choice1_2_1_1_1_1_1": "\033[7mNa hora que {} {} foi colocar o cara no carro ele gritou\033[m",
+    "choice1_2_1_1_1_1_1_1": "\033[7m{} {} então percebe que ele esta alterado\n\033[m",
+    "choice1_2_1_1_1_1_1_1_1": "\033[4;33mVoce faz oque\n 1 tenta conversar com ele\n 2 foje de la \n digite: \033[m",
+    "choice1_2_1_1_1_1_1_1_1_1": "\033[7m{} {} pergunta qual e o nome dele\n e veio a resposta\n\033[m",
+    "choice1_2_1_1_1_1_1_1_1_1_1": "\033[7m{} {} pergunta se ele sabia oque aconteceu\n e veio a resposta\n\033[m",
+    "choice1_2_1_1_1_1_1_1_1_1_1_1": "\033[7m{} {} pergunta se estava de carro\n e veio a resposta\n\033[m",
+    "choice1_2_1_1_1_1_1_1_1_1_1_1_1": "\033[7m{} {} então sente um cheiro de alcool e fala voce esta empreagado\n e veio a resposta\n\033[m",
+    "choice1_2_1_1_1_1_1_1_1_1_1_1_1_1": "\033[7mA pessoa se altera e parte para cima do {} e acaba ficando inconciete\n\033[m",
+    "choice1_2_1_1_1_1_1_1_1_1_1_1_1_1_1": "\033[7mFim de jogo\n voce não deve falar que a pessoa esta empreagada\033[m",
+    "choice1_2_1_1_1_1_2": "\033[7m Na hora que {} {} foi colocar o cara no lugar seguro\n\033[m",
+    "choice1_2_1_1_1_1_2_1": "\033[7m{} {} então percebe que ele esta alterado\n\033[m",
+    "choice1_2_1_1_1_1_2_1_1": "\033[4;33mVoce faz oque\n 1 tenta conversar com ele\n 2 foje de la \n digite: \033[m",
+    "choice1_2_1_1_1_1_2_1_1_1": "\033[7m{} {} pergunta qual e o nome dele\n e veio a resposta\n\033[m",
+    "choice1_2_1_1_1_1_2_1_1_1_1": "\033[7m{} {} pergunta se ele sabia oque aconteceu\n e veio a resposta\n\033[m",
+    "choice1_2_1_1_1_1_2_1_1_1_1_1": "\033[7m{} {} pergunta se estava de carro\n e veio a resposta\n\033[m",
+    "choice1_2_1_1_1_1_2_1_1_1_1_1_1": "\033[7m{} {} então sente um cheiro de alcool e fala voce esta empreagado\n e veio a resposta\n\033[m",
+    "choice1_2_1_1_1_1_2_1_1_1_1_1_1_1": "\033[7mA pessoa se altera e parte para cima do {} e acaba ficando inconciete\n\033[m",
+    "choice1_2_1_1_1_1_1_1_2_1_1_1_1_1_1": "\033[7mFim de jogo\n voce não deve falar que a pessoa esta empreagada\033[m",
+    "choice1_2_2": "\033[7m{} {} rouba a carteira e ouve um grito\033[m",
+    "choice1_2_2_1": "\033[7mNa hora vem varios policias\nconclusão voce e preso por roubo\nfim de jogo\033[m",
+
+}
+
+# Story 2 Texts
+STORY2_TEXTS = {
+    "intro": "{} {} esta querendo pegar o seu dinheiro na sua caderneta de polpança mas ve que a fila esta muito longa",
+    "choice2": "Oque voce faz\n 1 vai fazer outra coisa e voltar mas tarde\n 2 vai esperar na fila\n digite: ",
+    "choice2_1": "{} {} voltou mas tarde e viu que a fila tinha diminuido então {} foi para a fila\nmas {} não esperava que isso ia acontecer\num ladrão estava esperando\nassim que {} saiu o ladrão ja veio falando passa tudo",
+    "choice2_1_1": "Oque voce faz\n 1 corre o mais rapido que puder\n 2 entrega o dinheiro\n 3 grita para chamar a atenção dos quardas\n digite: ",
+    "choice2_1_1_1": "{} {} tentou fugir mas não conseguiu e acabou sendo atingido pela bala do ladrão que levou o dinheiro\nfim de jogo",
+    "choice2_1_1_2": "{} {} entrga o dinheiro pro ladrão so que ele te mata mesmo assim\nfim de jogo",
+    "choice2_1_1_3": "{} {} grita",
+    "choice2_1_1_3_1": "os guardas escutam e pegam o ladrão\n{} resebe uma recompença no valor de \nfim de jogo",
+    "choice2_2": "{} {} esperou",
+    "choice2_2_1": "{} {} esperou!",
+    "choice2_2_2": "{} {} esperou!!!!",
+    "choice2_2_3": "{} {} esperou!!!!!!",
+    "choice2_2_4": "ate que morreu de exastão",
+}
+
+# Story 3 Texts
+STORY3_TEXTS = {
+    "intro": "{} {} ve a chegada de um clinte que não parece ter muito dinheiro",
+    "choice3": "Oque voce faz\n 1 leva ele para uma mesa distante\n 2 coloca ele numa mesa com janela\n digite: ",
+    "choice3_1": "{} {} coloca ele na mesa distante e pergunta\no que voce quer para comer?\nele responde",
+    "choice3_1_1": "{} {} fica surpreso com o pedido mas anota mesmo assim\ndepois chega com o pedido\ndepois de comer voce fala quanto que custou",
+    "choice3_1_2": "{} {} fica mas surpreso ainda quando ele retira do seu boço um cartão e fala",
+    "choice3_2": "{} {} coloca ele na mesa com vista pela janela e pergunta\no que voce quer para comer?\nele responde",
+    "choice3_2_1": "{} {} perqunta\nalgo para beber?\nele responde",
+    "choice3_2_2": "depois dele ter se alimntando {} {} fala o valor da refeição",
+    "choice3_2_3": "e ele responde so tenho",
+}
