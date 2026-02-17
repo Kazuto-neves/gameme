5a0ecc8c6864cf5b1e4e4fc173fd0ea813776ded
Kazuto-neves <chrismneves@gmail.com>
2026-02-17 11:42:44 -0300

    Add Library_Text module and integrate story texts into Story1Add Library_Text module and integrate story texts into Story1

--- Game/Stories/Story2.py
+++ Game/Stories/Story2.py
@@ -1,106 +1,41 @@
-def Story(audio_player, nome, Sujeito):
-    print(
-        '\033[7m{} {} esta querendo pegar o seu dinheiro na sua caderneta de polpança mas ve que a fila esta muito longa\033[m'.format(
-            Sujeito, nome))
-    o2 = int(input(
-        '\033[4;33mOque voce faz\n 1 vai fazer outra coisa e voltar mas tarde\n 2 vai esperar na fila\n digite: \033[m'))
-    if o2 == 1:
-        print(
-            '\033[7m{} {} voltou mas tarde e viu que a fila tinha diminuido então {} foi para a fila\nmas {} não esperava que isso ia acontecer\num ladrão estava esperando\nassim que {} saiu o ladrão ja veio falando passa tudo\n\033[m'.format(
-                Sujeito, nome, Sujeito, nome, nome))
-        o21 = int(input(
-            '\033[4;33mOque voce faz\n 1 corre o mais rapido que puder\n 2 entrega o dinheiro\n 3 grita para chamar a atenção dos quardas\n digite: \033[m'))
-        if o21 == 1:
-            print(
-                '\033[7m{} {} tentou fugir mas não conseguiu e acabou sendo atingido pela bala do ladrão que levou o dinheiro\nfim de jogo\n\033[m'.format(
-                    Sujeito, nome))
-            audio_player.play('pega.mp3')
-        elif o21 == 2:
-            print(
-                '\033[7m{} {} entrga o dinheiro pro ladrão so que ele te mata mesmo assim\nfim de jogo\n\033[m'.format(
-                    Sujeito, nome))
-            audio_player.play('come.mp3')
-        else:
-            print('\033[7m{} {} grita\n\033[m'.format(Sujeito, nome))
-            audio_player.play('PegaLadrao.mp3')
-            print('\033[7mdigite qualquer coisa para continuar\033[m')
-            input("")
-            print(
-                '\033[7mos guardas escutam e pegam o ladrão\n{} resebe uma recompença no valor de \nfim de jogo\033[m'.format(
-                    nome))
-            audio_player.play('mil8.mp3')
-    else:
-        print('\033[7m{} {} esperou\n\033[m'.format(Sujeito, nome))
-        audio_player.play('1.mp3')
-        print('\033[7mdigite qualquer coisa para continuar\033[m')
-        input("")
-        print('\033[7m{} {} esperou!\n\033[m'.format(Sujeito, nome))
-        audio_player.play('2.mp3')
-        print('\033[7mdigite qualquer coisa para continuar\033[m')
-        input("")
-        print('\033[7m{} {} esperou!!!!\n\033[m'.format(Sujeito, nome))
-        audio_player.play('3.mp3')
-        print('\033[7mdigite qualquer coisa para continuar\033[m')
-        input("")
-        print('\033[7m{} {} esperou!!!!!!\n\033[m'.format(Sujeito, nome))
-        audio_player.play('2000.mp3')
-        print('\033[7mdigite qualquer coisa para continuar\033[m')
-        input("")
-        print('\033[7mate que morreu de exastão\n\033[m'.format(nome))
-        print('\033[7mdigite qualquer coisa para continuar\033[m')
-        input("")
-        print("""\033[;31m                                .,od88888888888bo,.
-                                    .d88888888888888888888888b.
-                                .d88888888888888888888888888888b.
-                               .d888888888888888888888888888888888b.
-                             .d8888888888888888888888888888888888888b.
-                            d88888888888888888888888888888888888888888b
-                           d8888888888888888888888888888888888888888888b
-                          d888888888888888888888888888888888888888888888
-                          8888888888888888888888888888888888888888888888
-                          8888888888888888888888888888888888888888888888
-                          8888888888888888888888888888888888888888888888
-                          Y88888888888888888888888888888888888888888888P
-                          "8888888888P'   "Y8888888888P"    "Y888888888"
-                           88888888P        Y88888888P        Y88888888
-                           Y8888888          ]888888P          8888888P
-                            Y888888          d888888b          888888P
-                             Y88888b        d88888888b        d88888P
-                              Y888888b.   .d88888888888b.   .d888888
-                               Y8888888888888888P Y8888888888888888
-                                888888888888888P   Y88888888888888
-                                "8888888888888[     ]888888888888"
-                                   "Y888888888888888888888888P"
-                                        "Y88888888888888P"
-                                     888b  Y8888888888P  d888
-                                     "888b              d888"
-                                      Y888bo.        .od888P
-                                       Y888888888888888888P
-                                        "Y88888888888888P"
-                                          "Y8888888888P"
-                  d8888bo.                  "Y888888P"                  .od888b
-                 888888888bo.                                        .od8888888
-                 "88888888888b.                                   .od888888888[
-                 d8888888888888bo.                              .od888888888888
-               d88888888888888888888bo.                     .od8888888888888888b
-               ]888888888888888888888888bo.            .od8888888888888888888888b=
-               888888888P" "Y888888888888888bo.     .od88888888888888P" "Y888888P=
-                Y8888P"           "Y888888888888bd888888888888P"            "Y8P
-                  ""                   "Y8888888888888888P"
-                                         .od8888888888bo.
-                                     .od888888888888888888bo.
-                                 .od8888888888P"  "Y8888888888bo.
-                              .od8888888888P"        "Y8888888888bo.
-                          .od88888888888P"              "Y88888888888bo.
-                .od888888888888888888P"                    "Y8888888888888888bo.
-               Y8888888888888888888P"                         "Y8888888888888888b=
-               888888888888888888P"                            "Y8888888888888888=
-                "Y888888888888888                               "Y88888888888888P=
-                     ""Y8888888P                                  "Y888888P"
-                        "Y8888P                                     Y888P""
+from Game.Ultils.Library_Text import STORY2_TEXTS, COMMON
 
-                                █▀▀ █▀▀█ ▀█░█▀ █▀▀ ░▀░ █▀▀█ █▀▀█
-                                █░░ █▄▄█ ░█▄█░ █▀▀ ▀█▀ █▄▄▀ █▄▄█
-                                ▀▀▀ ▀░░▀ ░░▀░░ ▀▀▀ ▀▀▀ ▀░▀▀ ▀░░▀
-                \033[m""")
+def Story(audio_player, nome, Sujeito):
+    print(STORY2_TEXTS["intro"].format(Sujeito, nome))
+    o2 = int(input(STORY2_TEXTS["choice2"]))
+    if o2 == 1:
+        print(STORY2_TEXTS["choice2_1"].format(Sujeito, nome, Sujeito, nome, nome))
+        o21 = int(input(STORY2_TEXTS["choice2_1_1"]))
+        if o21 == 1:
+            print(STORY2_TEXTS["choice2_1_1_1"].format(Sujeito, nome))
+            audio_player.play('pega.mp3')
+            print(COMMON["game_over"])
+        elif o21 == 2:
+            print(STORY2_TEXTS["choice2_1_1_2"].format(Sujeito, nome))
+            audio_player.play('come.mp3')
+            print(COMMON["game_over"])
+        else:
+            print(STORY2_TEXTS["choice2_1_1_3"].format(Sujeito, nome))
+            audio_player.play('PegaLadrao.mp3')
+            input(COMMON["continue"])
+            print(STORY2_TEXTS["choice2_1_1_3_1"].format(Sujeito, nome))
+            audio_player.play('mil8.mp3')
+            input(COMMON["game_over"])
+    else:
+        print(STORY2_TEXTS["choice2_2"].format(Sujeito, nome))
+        audio_player.play('1.mp3')
+        input(COMMON["continue"])
+        print(STORY2_TEXTS["choice2_2_1"].format(Sujeito, nome))
+        audio_player.play('2.mp3')
+        input(COMMON["continue"])
+        print(STORY2_TEXTS["choice2_2_2"].format(Sujeito, nome))
+        audio_player.play('3.mp3')
+        input(COMMON["continue"])
+        print(STORY2_TEXTS["choice2_2_3"].format(Sujeito, nome))
+        audio_player.play('2000.mp3')
+        input(COMMON["continue"])
+        print(STORY2_TEXTS["choice2_2_4"].format(Sujeito, nome))
+        input(COMMON["continue"])
+        print(STORY2_TEXTS["choice2_2_5"].format(Sujeito, nome))
         audio_player.play('caveira.mp3')
+        input(COMMON["game_over"])
--- Game/Ultils/Library_Text.py
+++ Game/Ultils/Library_Text.py
@@ -39,36 +39,93 @@ STORY1_TEXTS = {
     "choice1_2_1_1_1_1_2_1_1_1_1_1_1_1": "\033[7mA pessoa se altera e parte para cima do {} e acaba ficando inconciete\n\033[m",
     "choice1_2_1_1_1_1_1_1_2_1_1_1_1_1_1": "\033[7mFim de jogo\n voce não deve falar que a pessoa esta empreagada\033[m",
     "choice1_2_2": "\033[7m{} {} rouba a carteira e ouve um grito\033[m",
-    "choice1_2_2_1": "\033[7mNa hora vem varios policias\nconclusão voce e preso por roubo\nfim de jogo\033[m",
+    "choice1_2_2_1": "\033[7mNa hora vem varios policias\nconclusão voce e preso por roubo\n\033[m",
 
 }
 
 # Story 2 Texts
 STORY2_TEXTS = {
-    "intro": "{} {} esta querendo pegar o seu dinheiro na sua caderneta de polpança mas ve que a fila esta muito longa",
-    "choice2": "Oque voce faz\n 1 vai fazer outra coisa e voltar mas tarde\n 2 vai esperar na fila\n digite: ",
-    "choice2_1": "{} {} voltou mas tarde e viu que a fila tinha diminuido então {} foi para a fila\nmas {} não esperava que isso ia acontecer\num ladrão estava esperando\nassim que {} saiu o ladrão ja veio falando passa tudo",
-    "choice2_1_1": "Oque voce faz\n 1 corre o mais rapido que puder\n 2 entrega o dinheiro\n 3 grita para chamar a atenção dos quardas\n digite: ",
-    "choice2_1_1_1": "{} {} tentou fugir mas não conseguiu e acabou sendo atingido pela bala do ladrão que levou o dinheiro\nfim de jogo",
-    "choice2_1_1_2": "{} {} entrga o dinheiro pro ladrão so que ele te mata mesmo assim\nfim de jogo",
-    "choice2_1_1_3": "{} {} grita",
-    "choice2_1_1_3_1": "os guardas escutam e pegam o ladrão\n{} resebe uma recompença no valor de \nfim de jogo",
-    "choice2_2": "{} {} esperou",
-    "choice2_2_1": "{} {} esperou!",
-    "choice2_2_2": "{} {} esperou!!!!",
-    "choice2_2_3": "{} {} esperou!!!!!!",
-    "choice2_2_4": "ate que morreu de exastão",
+    "intro": "\033[7m{} {} esta querendo pegar o seu dinheiro na sua caderneta de polpança mas ve que a fila esta muito longa\033[m",
+    "choice2": "\033[4;33mOque voce faz\n 1 vai fazer outra coisa e voltar mas tarde\n 2 vai esperar na fila\n digite: \033[m",
+    "choice2_1": "\033[7m{} {} voltou mas tarde e viu que a fila tinha diminuido então {} foi para a fila\nmas {} não esperava que isso ia acontecer\num ladrão estava esperando\nassim que {} saiu o ladrão ja veio falando passa tudo\n\033[m",
+    "choice2_1_1": "\033[4;33mOque voce faz\n 1 corre o mais rapido que puder\n 2 entrega o dinheiro\n 3 grita para chamar a atenção dos quardas\n digite: \033[m",
+    "choice2_1_1_1": "\033[7m{} {} tentou fugir mas não conseguiu e acabou sendo atingido pela bala do ladrão que levou o dinheiro\n\033[m",
+    "choice2_1_1_2": "\033[7m{} {} entrga o dinheiro pro ladrão so que ele te mata mesmo assim\n\033[m",
+    "choice2_1_1_3": "\033[7m{} {} grita\n\033[m",
+    "choice2_1_1_3_1": "\033[7mos guardas escutam e pegam o ladrão\n{} resebe uma recompença no valor de \n",
+    "choice2_2": "\033[7m{} {} esperou\n\033[m",
+    "choice2_2_1": "\033[7m{} {} esperou!\n\033[m",
+    "choice2_2_2": "\033[7m{} {} esperou!!!!\n\033[m",
+    "choice2_2_3": "\033[7m{} {} esperou!!!!!!\n\033[m",
+    "choice2_2_4": "\033[7mate que morreu de exastão\n\033[m",
+    "choice2_2_5": """"\033[;31m                                .,od88888888888bo,.
+                                    .d88888888888888888888888b.
+                                .d88888888888888888888888888888b.
+                               .d888888888888888888888888888888888b.
+                             .d8888888888888888888888888888888888888b.
+                            d88888888888888888888888888888888888888888b
+                           d8888888888888888888888888888888888888888888b
+                          d888888888888888888888888888888888888888888888
+                          8888888888888888888888888888888888888888888888
+                          8888888888888888888888888888888888888888888888
+                          8888888888888888888888888888888888888888888888
+                          Y88888888888888888888888888888888888888888888P
+                          "8888888888P'   "Y8888888888P"    "Y888888888"
+                           88888888P        Y88888888P        Y88888888
+                           Y8888888          ]888888P          8888888P
+                            Y888888          d888888b          888888P
+                             Y88888b        d88888888b        d88888P
+                              Y888888b.   .d88888888888b.   .d888888
+                               Y8888888888888888P Y8888888888888888
+                                888888888888888P   Y88888888888888
+                                "8888888888888[     ]888888888888"
+                                   "Y888888888888888888888888P"
+                                        "Y88888888888888P"
+                                     888b  Y8888888888P  d888
+                                     "888b              d888"
+                                      Y888bo.        .od888P
+                                       Y888888888888888888P
+                                        "Y88888888888888P"
+                                          "Y8888888888P"
+                  d8888bo.                  "Y888888P"                  .od888b
+                 888888888bo.                                        .od8888888
+                 "88888888888b.                                   .od888888888[
+                 d8888888888888bo.                              .od888888888888
+               d88888888888888888888bo.                     .od8888888888888888b
+               ]888888888888888888888888bo.            .od8888888888888888888888b=
+               888888888P" "Y888888888888888bo.     .od88888888888888P" "Y888888P=
+                Y8888P"           "Y888888888888bd888888888888P"            "Y8P
+                  ""                   "Y8888888888888888P"
+                                         .od8888888888bo.
+                                     .od888888888888888888bo.
+                                 .od8888888888P"  "Y8888888888bo.
+                              .od8888888888P"        "Y8888888888bo.
+                          .od88888888888P"              "Y88888888888bo.
+                .od888888888888888888P"                    "Y8888888888888888bo.
+               Y8888888888888888888P"                         "Y8888888888888888b=
+               888888888888888888P"                            "Y8888888888888888=
+                "Y888888888888888                               "Y88888888888888P=
+                     ""Y8888888P                                  "Y888888P"
+                        "Y8888P                                     Y888P""
+
+                                █▀▀ █▀▀█ ▀█░█▀ █▀▀ ░▀░ █▀▀█ █▀▀█
+                                █░░ █▄▄█ ░█▄█░ █▀▀ ▀█▀ █▄▄▀ █▄▄█
+                                ▀▀▀ ▀░░▀ ░░▀░░ ▀▀▀ ▀▀▀ ▀░▀▀ ▀░░▀
+                \033[m""",
 }
 
 # Story 3 Texts
 STORY3_TEXTS = {
-    "intro": "{} {} ve a chegada de um clinte que não parece ter muito dinheiro",
-    "choice3": "Oque voce faz\n 1 leva ele para uma mesa distante\n 2 coloca ele numa mesa com janela\n digite: ",
-    "choice3_1": "{} {} coloca ele na mesa distante e pergunta\no que voce quer para comer?\nele responde",
-    "choice3_1_1": "{} {} fica surpreso com o pedido mas anota mesmo assim\ndepois chega com o pedido\ndepois de comer voce fala quanto que custou",
-    "choice3_1_2": "{} {} fica mas surpreso ainda quando ele retira do seu boço um cartão e fala",
-    "choice3_2": "{} {} coloca ele na mesa com vista pela janela e pergunta\no que voce quer para comer?\nele responde",
-    "choice3_2_1": "{} {} perqunta\nalgo para beber?\nele responde",
-    "choice3_2_2": "depois dele ter se alimntando {} {} fala o valor da refeição",
-    "choice3_2_3": "e ele responde so tenho",
+    "intro": "\033[7m{} {} ve a chegada de um clinte que não parece ter muito dinheiro\n\033[m",
+    "choice3": "Oque voce faz\n 1 leva ele para uma mesa distante\n 2 coloca ele numa mesa com janela\n digite: \033[m",
+    "choice3_1": "\033[7m{} {} coloca ele na mesa distante e pergunta\no que voce quer para comer?\nele responde\n\033[m",
+    "choice3_1_1": "\033[7m{} {} fica surpreso com o pedido mas anota mesmo assim\ndepois chega com o pedido\ndepois de comer voce fala quanto que custou\n\033[m",
+    "choice3_1_2": "\033[7m{} {} fica mas surpreso ainda quando ele retira do seu boço um cartão e fala\n\033[m",
+    "choice3_1_3": """\033[0;31;44m█▀▀▄ █▀▀█   █▀▀ █▀▀█ █▀▀ █▀▀▄ ░▀░ ▀▀█▀▀ █▀▀█   █▀▀█ █▀▀█ █▀▀█   █▀▀ █▀▀█ ▀█░█▀ █▀▀█ █▀▀█\033[m
+\033[0;31;44m█░░█ █░░█   █░░ █▄▄▀ █▀▀ █░░█ ▀█▀ ░░█░░ █░░█   █░░█ █░░█ █▄▄▀   █▀▀ █▄▄█ ░█▄█░ █░░█ █▄▄▀\033[m
+\033[0;31;44m▀░░▀ ▀▀▀▀   ▀▀▀ ▀░▀▀ ▀▀▀ ▀▀▀░ ▀▀▀ ░░▀░░ ▀▀▀▀   █▀▀▀ ▀▀▀▀ ▀░▀▀   ▀░░ ▀░░▀ ░░▀░░ ▀▀▀▀ ▀░▀▀\033[m""",
+    "choice3_2": "\033[7m{} {} coloca ele na mesa com vista pela janela e pergunta\no que voce quer para comer?\nele responde\n\033[m",
+    "choice3_2_1": "\033[7m{} {} perqunta\nalgo para beber?\nele responde\n\033[m",
+    "choice3_2_2": "\033[7mdepois dele ter se alimntando {} {} fala o valor da refeição\n\033[m",
+    "choice3_2_3": "\033[7me ele responde so tenho\n\033[m",
 }
